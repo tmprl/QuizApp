a4d1a5f8c2fd886fb8acce6a291f3d3cb2fef377
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-12-30 22:42:35 +0400

    feat: create questions variable, add question and answers to it in js

---
+++ script.js
@@ -0,0 +1,47 @@
+const questions = [
+    {
+        question: 'What is the capital of France?',
+        answers: [
+            {text: 'Berlin', correct: false},
+            {text: 'Madrid', correct: false},
+            {text: 'Paris', correct: true},
+            {text: 'Rome', correct: false},
+        ]
+    },
+    {
+        question: 'How many continents are there?',
+        answers: [
+            {text: '5', correct: false},
+            {text: '6', correct: false},
+            {text: '7', correct: true},
+            {text: '8', correct: false},
+        ]
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        answers: [
+            {text: 'Venus', correct: false},
+            {text: 'Jupiter', correct: false},
+            {text: 'Saturn', correct: false},
+            {text: 'Mars', correct: true},
+        ]
+    }
+    {
+        question: 'What is the largest ocean on Earth?',
+        answers: [
+            {text: 'Pacific Ocean', correct: true},
+            {text: 'Arctic Ocean', correct: false},
+            {text: 'Indian Ocean', correct: false},
+            {text: 'Atlantic Ocean', correct: false},
+        ]
+    },
+    {
+        question: 'Who painted the Mona Lisa?',
+        answers: [
+            {text: 'Vincent van Gogh', correct: false},
+            {text: 'Leonardo da Vinci', correct: true},
+            {text: 'Michelangelo', correct: false},
+            {text: 'Pablo Picasso', correct: false},
+        ]
+    }
+]
